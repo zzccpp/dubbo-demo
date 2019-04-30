98595c03dd81dd2ee7b8f9fea2c610c623d904da
zhongcp <1157723258@qq.com>
2019-04-30 17:22:54 +0800

    使用SpringBoot创建供服务项目

---
+++ springboot-provider/src/main/java/cn/zcp/demo/SpringbootProviderApplication.java
@@ -0,0 +1,12 @@
+package cn.zcp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProviderApplication.class, args);
+    }
+}
